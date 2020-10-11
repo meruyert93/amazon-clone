0d472e082277f1a05097ce59731fafc1411c3e37
Meruyert Karim <meruyert.karim@nu.edu.kz>
2020-10-11 21:54:52 +0200

    Basket further functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Checkout from "./Checkout";
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
@@ -42,9 +43,13 @@ function App() {
             <Route path="/login">
                 <Login />
             </Route>
+            <Route path="/checkout">
+                <Header />
+                <Checkout />
+            </Route>
             <Route path="/">
-              <Header />
-              <Home />
+                <Header />
+                <Home />
             </Route>
           </Switch>
         </Router>
